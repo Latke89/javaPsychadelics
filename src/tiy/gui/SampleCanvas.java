55e908dd39f33243beaec7162abad9107a718b62
Brett Gordon <gordonbrett89@gmail.com>
2016-08-29 14:04:00 -0400

    added filled ovals

--- src/tiy/gui/SampleCanvas.java
+++ src/tiy/gui/SampleCanvas.java
@@ -37,7 +37,7 @@ public class SampleCanvas extends Application {
 		graphicsContext.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
 		graphicsContext.setLineWidth(2);
 
-		drawShapes(graphicsContext);
+//		drawShapes(graphicsContext);
 
 		canvas.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
@@ -70,8 +70,9 @@ public class SampleCanvas extends Application {
 			public void handle(MouseEvent e) {
 				System.out.println("x: " + e.getX() + ", y: " + e.getY());
 				graphicsContext.strokeOval(e.getX(), e.getY(), strokeSize, strokeSize);
-				if (e.getClickCount() > 1) {
-					graphicsContext.clearRect(0, 0, DEFAULT_SCENE_WIDTH, DEFAULT_SCENE_HEIGHT);
+				if (e.isControlDown()) {
+					graphicsContext.fillOval(e.getX(), e.getY(), strokeSize, strokeSize);
+					graphicsContext.setFill(Color.color(Math.random(), Math.random(), Math.random()));
 				}
 			}
 		});
@@ -84,28 +85,28 @@ public class SampleCanvas extends Application {
 		primaryStage.show();
 	}
 
-	private void drawShapes(GraphicsContext gc) {
-		gc.setFill(Color.GREEN);
-		gc.setStroke(Color.BLUE);
-		gc.setLineWidth(5);
-		gc.strokeLine(40, 10, 10, 40);
-		gc.fillOval(10, 60, 30, 30);
-		gc.strokeOval(60, 60, 30, 30);
-		gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-		gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-		gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-		gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-		gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-		gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-		gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-		gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-		gc.fillPolygon(new double[]{10, 40, 10, 40},
-				new double[]{210, 210, 240, 240}, 4);
-		gc.strokePolygon(new double[]{60, 90, 60, 90},
-				new double[]{210, 210, 240, 240}, 4);
-		gc.strokePolyline(new double[]{110, 140, 110, 140},
-				new double[]{210, 210, 240, 240}, 4);
-	}
+//	private void drawShapes(GraphicsContext gc) {
+//		gc.setFill(Color.GREEN);
+//		gc.setStroke(Color.BLUE);
+//		gc.setLineWidth(5);
+//		gc.strokeLine(40, 10, 10, 40);
+//		gc.fillOval(10, 60, 30, 30);
+//		gc.strokeOval(60, 60, 30, 30);
+//		gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+//		gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+//		gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+//		gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+//		gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+//		gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+//		gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+//		gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+//		gc.fillPolygon(new double[]{10, 40, 10, 40},
+//				new double[]{210, 210, 240, 240}, 4);
+//		gc.strokePolygon(new double[]{60, 90, 60, 90},
+//				new double[]{210, 210, 240, 240}, 4);
+//		gc.strokePolyline(new double[]{110, 140, 110, 140},
+//				new double[]{210, 210, 240, 240}, 4);
+//	}
 
 	public static void main(String[] args) {
 		launch(args);
